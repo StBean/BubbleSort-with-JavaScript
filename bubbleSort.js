01d3c4a139ddbc339a6c4148c593b9e910eaa24a
Anthony Poblacion <StBean@users.noreply.github.com>
2018-04-25 13:24:40 -0700

    Create bubbleSort.js

---
+++ bubbleSort.js
@@ -0,0 +1,13 @@
+// BubbleSort implemented with Javascript
+for(var i = 0; i < data.length; i++){
+  for(var j = i + 1; j < data.length - i; j++){
+    if(data[i] < data[j]{
+      var temp = data[i];
+      data[i] = data[j];
+      data[j] = temp;
+      }
+   }
+};
+return data;
+
+// temp file, may be errors
