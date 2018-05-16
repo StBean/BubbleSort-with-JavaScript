5aaf4f1e93fde8f74168630206704eae73af482a
Anthony Poblacion <StBean@users.noreply.github.com>
2018-05-16 02:19:10 -0700

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,13 +1,13 @@
 // BubbleSort implemented with Javascript
-function bubbleSort(input){	
-	for(var i = 0; i < data.length - 1; i++){
-		for(var j = 0; j < data.length - 1 - i; j++){
-			if(data[j + 1] <= data[j]){
-				var temp = data[j];
-				data[j] = data[j + 1];
-				data[j + 1] = temp;
-			}
-		}
-	}
+function bubbleSort(data){	
+  for(var i = 0; i < data.length - 1; i++){
+    for(var j = 0; j < data.length - 1 - i; j++){
+      if(data[j + 1] <= data[j]){
+	var temp = data[j];
+	data[j] = data[j + 1];
+	data[j + 1] = temp;
+      }
+    }
+  }
 };
 
